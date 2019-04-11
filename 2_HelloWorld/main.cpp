5a2e7c8e5536f1fc63377c79c395eef3326f7c6c
parinyad123 <sci.equ@gmail.com>
2019-04-11 08:49:06 +0700

    increate debud

--- 2_HelloWorld/main.cpp
+++ 2_HelloWorld/main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
 
+#include<QDebug>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    qDebug() <<  "Hellow Qt5 C++ Application";
     return a.exec();
 }
