12bc253b7e6f4a70c2238027d4eeaf92d82e38fb
parinyad123 <sci.equ@gmail.com>
2019-04-11 19:29:32 +0700

    CheckBox finish

--- 8_CheckBox/dialog.cpp
+++ 8_CheckBox/dialog.cpp
@@ -6,9 +6,21 @@ Dialog::Dialog(QWidget *parent) :
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+
+    ui->checkBox->setChecked(true);
 }
 
 Dialog::~Dialog()
 {
     delete ui;
+}
+
+void Dialog::on_pushButton_clicked()
+{
+    if(ui->checkBox->isChecked()) {
+        ui->label->setText("Yes I like Sport");
+    }else {
+        ui->label->setText("No I Dom't like sport");
+    }
+
 }
--- 8_CheckBox/dialog.h
+++ 8_CheckBox/dialog.h
@@ -15,6 +15,9 @@ public:
     explicit Dialog(QWidget *parent = nullptr);
     ~Dialog();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::Dialog *ui;
 };
